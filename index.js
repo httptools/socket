9368d6eecb69f4299717eb0ddd589b989a638b09
HTTP Tools <148354584+httptools@users.noreply.github.com>
2024-01-10 15:27:53 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (data) => {
+        console.log('message: ' + data.text);
+        io.emit('chat message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
